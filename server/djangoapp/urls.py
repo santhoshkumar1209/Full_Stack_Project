8201c198526601d628d29d58e9cfae68b9a2fc1e
Santhosh Kumar S <Santhoshsrs1209@gmail.com>
2026-06-25 11:27:18 -0400

    Updated Django Models

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,9 +11,11 @@ urlpatterns = [
     # path for login
     path(route='login', view=views.login_user, name='login'),
 
-    path (route='logout', view=views.logout, name='logout'),
+    path(route='logout', view=views.logout, name='logout'),
 
-    path (route='register', view=views.registration, name='register'),
+    path(route='register', view=views.registration, name='register'),
+
+    path(route='get_cars', view=views.get_cars, name='get_cars'),
 
     # path for dealer reviews view
 
